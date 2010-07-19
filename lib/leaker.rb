7fcda97100116cc4a3a1d46ae2ad49d0c6da0f70
Marcello Barnaba <vjt@openssl.it>
2010-07-19 17:40:06 +0200

    More code documentation and warnings

--- lib/leaker.rb
+++ lib/leaker.rb
@@ -1,20 +1,36 @@
 module Panmind
   module Leaker
     module PluginMethods
+      # == DANGER, WILL ROBINSON! ==
+      #
+      # This code is evil - if you feel you need it, you're probably
+      # overlooking Rails' features such as +accepts_nested_attributes_for+
+      #
+      # Try to avoid it if possible, it is here only because it does
+      # interesting things extending rails behaviours.
+      #
+      # == Usage ==
+      #
       # Leaks the given controller +method+ to the model classes array
       # defined in the <tt>:to</tt> option. E.g.:
       #
-      # class FooController < ApplicationController
-      #   leaks :current_user, :to => [Bar]
-      # end
+      #   class FooController < ApplicationController
+      #     leaks :current_user, :to => [Bar]
+      #   end
       #
       # During the request cycle, an instance of the Bar model will
       # have access to the value returned by the :current_user method
       # in the controller.
       #
       # The special <tt>:all</tt> symbol, passed to the <tt>:to</tt>
-      # option, will extend the leak method to *all* ActiveRecord::Base
-      # and CouchRest::ExtendedDocument instances. Use with care!
+      # option, will extend the leak method to *all* +ActiveRecord::Base+
+      # and +CouchRest::ExtendedDocument+ (if defined) instances.
+      #
+      # Use with care! Every time you leak a controller method to a model
+      # the Flying Spaghetti Monster kills a kitten. You don't want kittens
+      # die, don't you?
+      #
+      #   - vjt  Mon Jul 19 17:39:05 CEST 2010
       #
       def leaks(method, options)
         controller = self
@@ -65,6 +81,12 @@ module Panmind
     end
 
     module ControllerMethods
+      # Returns the currently leaked methods and the respective models they were
+      # leaked from, in an Hash form.
+      #
+      # E.g. if the PostsController leaks :current_user, :to => Post, then
+      # PostsController.leakages #=> {:current_user => [Post]}
+      #
       def leakages
         read_inheritable_attribute(:leakages) || {}
       end
